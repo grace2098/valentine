7edbcb274f7e259d667b8b051ecac00b9359fd6b
olachi <olachiokoro5152@gmail.com>
2026-02-01 15:09:55 +0100

    edited the way image was called

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import confetti from "canvas-confetti";
 import "./App.css";
+import funnyDog from "../src/assets/funny picture with dog.jpg";
+import oydek from "../src/assets/öydek.jpg";
+import dogi from "../src/assets/Dogi.jpg";
+import screenshot from "../src/assets/Screenshot 2026-02-01 130711.png";
+import meme from "../src/assets/Meme.jpg";
+import perrito from "../src/assets/Perrito triste (1).jpg";
+import jajajaja from "../src/assets/Jajajaja.jpg";
+import mePhone from "../src/assets/Me_ “when someone removes my phone ku charger”.jpg";
+import funCat from "../src/assets/the fun cat.jpg";
 
 function App() {
   const [noCount, setNoCount] = useState(0);
@@ -9,15 +18,15 @@ function App() {
   const [noPos, setNoPos] = useState({ top: "40px", left: "120px" });
 
   const images = [
-    "../public/funny picture with dog.jpg",
-    "../public/öydek.jpg",
-    "../public/Dogi.jpg",
-    "../public/Screenshot 2026-02-01 130711.png",
-    "../public/Meme.jpg",
-    "../public/Perrito triste (1).jpg",
-    "../public/Jajajaja.jpg",
-    "../public/Me_ “when someone removes my phone ku charger”.jpg",
-    "../public/the fun cat.jpg",
+    funnyDog,
+    oydek,
+    dogi,
+    screenshot,
+    meme,
+    perrito,
+    jajajaja,
+    mePhone,
+    funCat,
   ];
 
   const messages = [
@@ -58,34 +67,33 @@ function App() {
     shootCanyonLong();
   };
 
- 
-const shootCanyonLong = () => {
-  for (let i = 0; i < 3; i++) {
-    // left corner
-    confetti({
-      particleCount: 150,
-      angle: 60,
-      spread: 60,
-      origin: { x: 0, y: 1 },
-      scalar: 1.5,
-      gravity: 0.3,  // float slower
-      decay: 0.92,   // shrink slower
-      ticks: 300     // last longer
-    });
+  const shootCanyonLong = () => {
+    for (let i = 0; i < 3; i++) {
+      // left corner
+      confetti({
+        particleCount: 150,
+        angle: 60,
+        spread: 60,
+        origin: { x: 0, y: 1 },
+        scalar: 1.5,
+        gravity: 0.3, // float slower
+        decay: 0.92, // shrink slower
+        ticks: 300, // last longer
+      });
 
-    // right corner
-    confetti({
-      particleCount: 150,
-      angle: 120,
-      spread: 60,
-      origin: { x: 1, y: 1 },
-      scalar: 1.5,
-      gravity: 0.3,
-      decay: 0.92,
-      ticks: 300
-    });
-  }
-};
+      // right corner
+      confetti({
+        particleCount: 150,
+        angle: 120,
+        spread: 60,
+        origin: { x: 1, y: 1 },
+        scalar: 1.5,
+        gravity: 0.3,
+        decay: 0.92,
+        ticks: 300,
+      });
+    }
+  };
 
   if (accepted) {
     return (
